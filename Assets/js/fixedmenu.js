81d77a3e3bef30d158355c490a746aa701c66375
Lauryn McMichael <mcm.lacm@gmail.com>
2021-04-07 17:58:51 -0400

    fixed repo

---
+++ Assets/js/fixedmenu.js
@@ -0,0 +1,15 @@
+var route = document.getElementById("route")
+var street1 = document.getElementById("street1")
+var street2 = document.getElementById("street2")
+const search = document.getElementById("search")
+
+
+
+function currentRoute() {
+    route.textContent = street1.value + " to " + street2.value
+}
+
+search.addEventListener("click", function(event) { 
+    event.preventDefault()  
+    currentRoute()
+})

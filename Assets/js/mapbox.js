c49ec9af8066f67f380f88ce47fd2dbb8fe98e53
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-04-07 19:44:34 -0400

    something.

--- Assets/js/mapbox.js
+++ Assets/js/mapbox.js
@@ -7,7 +7,8 @@ var endLocation;
 // var endLocation = $("#endLocation");
 
 var submit = $("#search");
-
+var image = $("#image");
+var ranking = $("#ranking");
 var mapboxglaccessToken = 'pk.eyJ1Ijoid2luZ2VoIiwiYSI6ImNrbXMwYnZxaTBjcXkybm14enczN281ZXEifQ.bsBm9JlGjhos_-74rAL1uA';
 
 // listen for click event
@@ -56,7 +57,10 @@ function getRoute(start, end) {
   //var url = 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
   
   var url = 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
-  
+  var collisionsByLocation = "https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/Traffic_Collisions_by_Location_2015_to_2019/FeatureServer/0/query?where=1%3D1&outFields=Location,Total_Collisions,F2016_Total,F2017_Total,F2018_Total,F2019_Total,Total_Pedestrians,F2015_Pedestrians,F2016_Pedestrians,F2017_Pedestrians,F2018_Pedestrians,F2019_Pedestrians,F2015_Total&outSR=4326&f=json"
+  var intersectionVolume = "https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/Transportation_Intersection_Volumes_2019/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+  var tabularCollisionData = "https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/2019_Tabular_Transportation_Collision_Data/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+
   // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
   var req = new XMLHttpRequest();
   req.open('GET', url, true);
@@ -72,77 +76,7 @@ function getRoute(start, end) {
 		coordinates: route
 	  }
 	};
-	// if the route already exists on the map, reset it using setData
-	if (map.getSource('route')) {
-	  map.getSource('route').setData(geojson);
-	} else { // otherwise, make a new request
-	  map.addLayer({
-		id: 'route',
-		type: 'line',
-		source: {
-		  type: 'geojson',
-		  data: {
-			type: 'Feature',
-			properties: {},
-			geometry: {
-			  type: 'LineString',
-			  coordinates: geojson
-			}
-		  }
-		},
-		layout: {
-		  'line-join': 'round',
-		  'line-cap': 'round'
-		},
-		paint: {
-		  'line-color': '#3887be',
-		  'line-width': 5,
-		  'line-opacity': 0.75
-		}
-	  });
-	}
-	// add turn instructions here at the end
-			// get the sidebar and add the instructions
-			var instructions = document.getElementById('instructions');
-  var steps = data.legs[0].steps;
-  
-  var tripInstructions = [];
-  for (var i = 0; i < steps.length; i++) {
-  tripInstructions.push('<br><li>' + steps[i].maneuver.instruction) + '</li>';
-  instructions.innerHTML = '<br><span class="duration">Trip duration: ' + Math.floor(data.duration / 60) + ' min </span>' + tripInstructions;
-  };
-  };
-  req.send();
-  }
-});
-
-
-
-// function getDirections (){
-
-// 	var url = 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
-	var collisionsByLocation = "https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/Traffic_Collisions_by_Location_2015_to_2019/FeatureServer/0/query?where=1%3D1&outFields=Location,Total_Collisions,F2016_Total,F2017_Total,F2018_Total,F2019_Total,Total_Pedestrians,F2015_Pedestrians,F2016_Pedestrians,F2017_Pedestrians,F2018_Pedestrians,F2019_Pedestrians,F2015_Total&outSR=4326&f=json"
-    var intersectionVolume = "https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/Transportation_Intersection_Volumes_2019/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-    var tabularCollisionData = "https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/2019_Tabular_Transportation_Collision_Data/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-
-// 	  // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-// 	  var req = new XMLHttpRequest();
-// 	  req.open('GET', url, true);
-// 	  req.onload = function() {
-// 		var json = JSON.parse(req.response);
-// 		var data = json.routes[0];
-// 		var route = data.geometry.coordinates;
-// 		var geojson = {
-// 		  type: 'Feature',
-// 		  properties: {},
-// 		  geometry: {
-// 			type: 'LineString',
-// 			coordinates: route
-// 		  }
-// 		};
-	
-// 		//RYANS KINGDOM START
-var intersectionArr = [];
+	var intersectionArr = [];
 
 for (var i = 0; i < data.legs[0].steps.length; i++) {
   var rounded = math.round(data.legs[0].steps[i].intersections[0].location, 3);
@@ -199,25 +133,93 @@ Promise.all ([
 	
 	if (uniqueCounterArr.length >= 9) {
 	  console.log("Kim")
-	  // image.attr("src", "https://upload.wikimedia.org/wikipedia/commons/e/e6/Kim_Kardashian_2019.jpg")
-	  // ranking.text("Your route is a Kim. Take necessary precautions.")
+	  image.attr("src", "https://upload.wikimedia.org/wikipedia/commons/e/e6/Kim_Kardashian_2019.jpg")
+	  ranking.text("Your route is a Kim. Take necessary precautions.")
 	   
 	} else if (uniqueCounterArr.length >= 6) {
-	  // image.attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Khloe_Kardashian_Glamour_2.png/220px-Khloe_Kardashian_Glamour_2.png")
+	  image.attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Khloe_Kardashian_Glamour_2.png/220px-Khloe_Kardashian_Glamour_2.png")
 	  console.log("Khloe")
-	  // ranking.text("Your route is a Khloe. Relatively safe.")
+	  ranking.text("Your route is a Khloe. Relatively safe.")
 	 
 	} else if (uniqueCounterArr.length >= 3) {
-	  // image.attr("src", "https://upload.wikimedia.org/wikipedia/commons/1/1a/Kourtney_Kardashian_2_2009.jpg")
+	  image.attr("src", "https://upload.wikimedia.org/wikipedia/commons/1/1a/Kourtney_Kardashian_2_2009.jpg")
 	  console.log("Kourtney")
-	  // ranking.text("Your route is a Kourtney. It very safe")
+	  ranking.text("Your route is a Kourtney. It very safe")
 	 
 	}  else  {
-	  // image.attr("src", "http://www.gstatic.com/tv/thumb/persons/616912/616912_v9_ba.jpg")
+	  image.attr("src", "http://www.gstatic.com/tv/thumb/persons/616912/616912_v9_ba.jpg")
 	  console.log("Rob")
-	  // ranking.text("Your route is a Rob. No problems here!")
+	  ranking.text("Your route is a Rob. No problems here!")
 	};
 });
+
+	// if the route already exists on the map, reset it using setData
+	if (map.getSource('route')) {
+	  map.getSource('route').setData(geojson);
+	} else { // otherwise, make a new request
+	  map.addLayer({
+		id: 'route',
+		type: 'line',
+		source: {
+		  type: 'geojson',
+		  data: {
+			type: 'Feature',
+			properties: {},
+			geometry: {
+			  type: 'LineString',
+			  coordinates: geojson
+			}
+		  }
+		},
+		layout: {
+		  'line-join': 'round',
+		  'line-cap': 'round'
+		},
+		paint: {
+		  'line-color': '#3887be',
+		  'line-width': 5,
+		  'line-opacity': 0.75
+		}
+	  });
+	}
+	// add turn instructions here at the end
+			// get the sidebar and add the instructions
+			var instructions = document.getElementById('instructions');
+  var steps = data.legs[0].steps;
+  
+  var tripInstructions = [];
+  for (var i = 0; i < steps.length; i++) {
+  tripInstructions.push('<br><li>' + steps[i].maneuver.instruction) + '</li>';
+  instructions.innerHTML = '<br><span class="duration">Trip duration: ' + Math.floor(data.duration / 60) + ' min </span>' + tripInstructions;
+  };
+  };
+  req.send();
+  }
+});
+
+
+
+// function getDirections (){
+
+// 	var url = 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+	
+// 	  // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+// 	  var req = new XMLHttpRequest();
+// 	  req.open('GET', url, true);
+// 	  req.onload = function() {
+// 		var json = JSON.parse(req.response);
+// 		var data = json.routes[0];
+// 		var route = data.geometry.coordinates;
+// 		var geojson = {
+// 		  type: 'Feature',
+// 		  properties: {},
+// 		  geometry: {
+// 			type: 'LineString',
+// 			coordinates: route
+// 		  }
+// 		};
+	
+// 		//RYANS KINGDOM START
 
 
 // 		//RYANS KINGDOM END

4eea3b4a4395d5e463c1500546894c77aa815e91
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-04-08 14:41:57 -0400

    menu goes away now.

--- Assets/js/fixedmenu.js
+++ Assets/js/fixedmenu.js
@@ -2,12 +2,12 @@ var route = document.getElementById("route")
 var street1 = document.getElementById("street1")
 var street2 = document.getElementById("street2")
 const search = document.getElementById("search")
-
+var menu = document.querySelector(".menu")
 
 
 function currentRoute() {
     route.textContent = street1.value + " to " + street2.value
-    
+    menu.style.visibility = "hidden";
 }
 
 search.addEventListener("click", function(event) { 
